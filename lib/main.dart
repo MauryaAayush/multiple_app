18500d020f2eb80d337fc8e117ffc616f8d1e3f5
Admin <maayush235@gmail.com>
2024-07-21 11:32:19 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'Login_APP/views/login_page.dart';
 import 'TODO_APP/Views/HomeScreen/todo_screen.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: LoginPage()
