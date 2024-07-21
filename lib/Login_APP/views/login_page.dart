88f446468e86118966f6d69386ad38a1463c9466
Admin <maayush235@gmail.com>
2024-07-21 15:26:16 +0530

    Radhey Radhey

--- lib/Login_APP/views/login_page.dart
+++ lib/Login_APP/views/login_page.dart
@@ -13,56 +13,85 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          // Container(
-          //   height: double.infinity,
-          //   width: double.infinity,
-          //   decoration: BoxDecoration(
-          //     image: DecorationImage(
-          //       image: AssetImage('assets/images/pexels-photo-807598.jpeg'),
-          //       fit: BoxFit.cover,
-          //     ),
-          //   ),
-          // ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              child: Form(
-                key: loginController.formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 40),
-                    Image.asset(
-                      'assets/images/pexels-photo-807598.jpeg',
-                      height: 150,
+          SingleChildScrollView(
+            child: Form(
+              key: loginController.formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 300,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomRight: Radius.elliptical(150, 150)),
+                        image: DecorationImage(
+                            image: AssetImage(
+                              'assets/images/pexels-photo-807598.jpeg',
+                            ),
+                            fit: BoxFit.cover)),
+                  ),
+                  SizedBox(height: 40),
+                  Text(
+                    'Welcome Back',
+                    style: GoogleFonts.ubuntu(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF40744D),
                     ),
-                    SizedBox(height: 40),
-                    Text(
-                      'Welcome Back',
-                      style: GoogleFonts.ubuntu(
-                        fontSize: 32,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xFF40744D),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Login to your account',
+                    style: GoogleFonts.roboto(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    child: TextFormField(
+                      controller: loginController.emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Full Name',
+                        labelStyle: TextStyle(color: Color(0xFF40744D),),
+                        prefixIcon: Icon(Icons.person, color: Color(0xFF40744D),),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
+                      validator: (value) {
+                        return loginController.validateEmail(value);
+                      },
                     ),
-                    SizedBox(height: 5),
-                    Text(
-                      'Login to your account',
-                      style: GoogleFonts.roboto(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    AnimatedContainer(
+                  ),
+                  SizedBox(height: 20),
+                  Obx(
+                    () => AnimatedContainer(
                       duration: Duration(milliseconds: 300),
                       child: TextFormField(
-                        controller: loginController.emailController,
+                        controller: loginController.passwordController,
                         decoration: InputDecoration(
-                          labelText: 'Full Name',
-                          labelStyle: TextStyle(color: Colors.green),
-                          prefixIcon: Icon(Icons.person, color: Colors.green),
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Color(0xFF40744D)),
+                          prefixIcon: Icon(Icons.lock, color: Color(0xFF40744D)),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              loginController.isPasswordVisible.value
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Color(0xFF40744D),
+                            ),
+                            onPressed: () {
+                              loginController.isPasswordVisible.value =
+                                  !loginController.isPasswordVisible.value;
+                            },
+                          ),
                           filled: true,
                           fillColor: Colors.white,
                           border: OutlineInputBorder(
@@ -70,118 +99,84 @@ class LoginPage extends StatelessWidget {
                             borderSide: BorderSide.none,
                           ),
                         ),
+                        obscureText: !loginController.isPasswordVisible.value,
                         validator: (value) {
-                          return loginController.validateEmail(value);
+                          return loginController.validatePassword(value);
                         },
                       ),
                     ),
-                    SizedBox(height: 20),
-                    Obx(
-                          () => AnimatedContainer(
-                        duration: Duration(milliseconds: 300),
-                        child: TextFormField(
-                          controller: loginController.passwordController,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.green),
-                            prefixIcon: Icon(Icons.lock, color: Colors.green),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                loginController.isPasswordVisible.value
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                                color: Colors.green,
-                              ),
-                              onPressed: () {
-                                loginController.isPasswordVisible.value =
-                                !loginController.isPasswordVisible.value;
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Obx(
+                            () => Checkbox(
+                              value: loginController.rememberMe.value,
+                              onChanged: (value) {
+                                loginController.rememberMe.value = value!;
                               },
-                            ),
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: BorderSide.none,
+                              activeColor: Colors.green,
                             ),
                           ),
-                          obscureText: !loginController.isPasswordVisible.value,
-                          validator: (value) {
-                            return loginController.validatePassword(value);
-                          },
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Obx(
-                                  () => Checkbox(
-                                value: loginController.rememberMe.value,
-                                onChanged: (value) {
-                                  loginController.rememberMe.value = value!;
-                                },
-                                activeColor: Colors.green,
-                              ),
-                            ),
-                            Text(
-                              'Remember Me',
-                              style: TextStyle(color: Colors.green),
-                            ),
-                          ],
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            // Add your forgot password functionality here
-                          },
-                          child: Text(
-                            'Forgot Password?',
+                          Text(
+                            'Remember Me',
                             style: TextStyle(color: Colors.green),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      child: ElevatedButton(
+                        ],
+                      ),
+                      TextButton(
                         onPressed: () {
-                          loginController.login();
+                          // Add your forgot password functionality here
                         },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          padding: EdgeInsets.symmetric(
-                              vertical: 16, horizontal: 32),
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.green),
                         ),
-                        child: Text('Login',style: TextStyle(color: Colors.white),),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        loginController.login();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have an account?",
-                          style: TextStyle(color: Colors.grey),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(color: Colors.green),
                         ),
-                        TextButton(
-                          onPressed: () {
-
-                          },
-                          child: Text(
-                            'Sign up',
-                            style: TextStyle(color: Colors.green),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'Login_APP/views/login_page.dart';
 import 'TODO_APP/Views/HomeScreen/todo_screen.dart';
 
 void main() {
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // Make the status bar transparent
+    statusBarIconBrightness: Brightness.dark, // Set status bar icons to dark for better visibility
+  ));
+
   runApp(const MyApp());
 }
 
