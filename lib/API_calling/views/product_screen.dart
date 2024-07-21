008c491e5aeb249f1975c5179fc63ad82c2e50d8
Admin <maayush235@gmail.com>
2024-07-21 16:47:15 +0530

    Radhey Radhey

--- lib/API_calling/views/product_screen.dart
+++ lib/API_calling/views/product_screen.dart
@@ -18,81 +18,106 @@ class ProductPage extends StatelessWidget {
         if (controller.isLoading.value) {
           return Center(child: CircularProgressIndicator());
         } else if (controller.productModel != null) {
-          return GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // Number of items per row
-              crossAxisSpacing: 10.0,
-              mainAxisSpacing: 10.0,
-              childAspectRatio: 0.75, // Adjust the aspect ratio as needed
-            ),
-            itemCount: controller.productModel?.products?.length ?? 0,
-            itemBuilder: (context, index) {
-              var product = controller.productModel!.products![index];
-              return Card(
-                elevation: 5,
+          return Column(
+            children: [
+              Container(
+
+                padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                height: 50,
+                width: 250,
                 margin: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Image.network(
-                        product.thumbnail ?? '',
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  'Your Container',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Number of items per row
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                    childAspectRatio: 0.75, // Adjust the aspect ratio as needed
+                  ),
+                  itemCount: controller.productModel?.products?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    var product = controller.productModel!.products![index];
+                    return Card(
+                      elevation: 5,
+                      margin: EdgeInsets.all(10),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            product.title ?? '',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            'Brand: ${product.brand ?? ''}',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12,
+                          Expanded(
+                            child: Image.network(
+                              product.thumbnail ?? '',
+                              fit: BoxFit.cover,
+                              width: double.infinity,
                             ),
                           ),
-                          SizedBox(height: 4),
-                          Text(
-                            '\$${product.price?.toStringAsFixed(2) ?? ''}',
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontSize: 14,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  product.title ?? '',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  'Brand: ${product.brand ?? ''}',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  '\$${product.price?.toStringAsFixed(2) ?? ''}',
+                                  style: TextStyle(
+                                    color: Colors.green,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                      size: 14,
+                                    ),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      product.rating?.toString() ?? '',
+                                      style: TextStyle(fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                          ),
-                          SizedBox(height: 4),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 14,
-                              ),
-                              SizedBox(width: 4),
-                              Text(
-                                product.rating?.toString() ?? '',
-                                style: TextStyle(fontSize: 12),
-                              ),
-                            ],
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+            ],
           );
         } else {
           return Center(child: Text('No products found'));
